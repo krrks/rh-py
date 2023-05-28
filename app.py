90b3b4c18ba441f70701b1f2da5b2833fae8ad06
krrks <25857593+krrks@users.noreply.github.com>
2023-05-28 22:53:41 +0800

    Update app.py
    1

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-import time
-time.sleep(3600)
+from flask import Flask, send_file
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route("/<file_name>", methods=["GET"])
+def get_file(file_name):
+    if len(file_name) > 0:
+        return send_file(f"./{file_name}", attachment_filename=file_name)
+    return "file not found."
+
+if __name__ == '__main__':
+    app.run()
