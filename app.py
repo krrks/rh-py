9f4e7f43f00bb0da14012709ebaea5678b80069a
krrks <25857593+krrks@users.noreply.github.com>
2023-05-30 17:48:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, send_file
 app = Flask(__name__)
 
+def gpt_query(str_q):
+    from EdgeGPT import Query, Cookie
+    q = Query("你是谁？用python代码给出回答")
+    return q.sources
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route("/<file_name>", methods=["GET"])
-def get_file(file_name):
-    if len(file_name) > 0:
-        return send_file(f"./{file_name}")
-    return "file not found."
+@app.route("/<str_q>", methods=["GET"])
+def get_gpt(str_q):
+    if len(str_q) > 0:
+        return gpt_query(str_q.replace("_"," "))
+    return "question not found."
+
+# @app.route("/<file_name>", methods=["GET"])
+# def get_file(file_name):
+#     if len(file_name) > 0:
+#         return send_file(f"./{file_name}")
+#     return "file not found."
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080) #host="0.0.0.0",port=8080
